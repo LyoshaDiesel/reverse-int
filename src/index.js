5a63df67a6611a4112ef357b253e621aca4e3876
LyoshaDiesel <akljjf@gmail.com>
2024-08-19 16:06:27 +0300

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+if (n < 0) {
+    const positiveNumber = n * -1;
+    return positiveNumber.toString().split('').reverse().join('');
+} else {
+    return n.toString().split('').reverse().join('');
+}
 }
